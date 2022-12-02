eee214fb1a8b2c240c717a8a8b7ff4ba6bed3f3c
8moses <506869@vistacollege.nl>
2022-12-02 14:32:58 +0100

    bbbbbbbbbb
    vvvvvvvvvvvv

--- rekenmachine.py
+++ rekenmachine.py
@@ -2,7 +2,7 @@ Num1 = float(input("Kies je eerste getal: "))
 while True:
   opr = input("kies hier je operator (+, -, , /, =): ")
 
-  if "+" in opr:
+  if "+" in opr: 
     Num2 = input("Kies je nieuwe getal: ")
     Num1 = Num1 + float(Num2)
     print("tussenuitkomst is", Num1)
